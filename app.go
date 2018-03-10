d66a1fd5ef4cf1f62836ee9b2548854ec9edaa1a
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-10 18:19:48 -0500

    Usando una ruta en una funcion

--- app.go
+++ app.go
@@ -2,12 +2,19 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func GetPong(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "ping",
 		})
 	})
+	r.GET("/pong", GetPong)
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
