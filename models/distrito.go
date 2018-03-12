e6a4e8fa9a3e10a83a46832ffcd4cb40e093d078
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-12 17:59:43 -0500

    Listar distritos de provincia

---
+++ models/distrito.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"encoding/json"
+	"strconv"
+)
+
+type Distrito struct {
+	ID           int    `gorm:"primary_key" json:id`
+	Nombre       string `gorm:"column:nombre" json:nombre`
+	Provincia_id int    `gorm:"column:provincia_id"`
+}
+
+func (Distrito) TableName() string {
+	return "distritos"
+}
+
+func (distrito *Distrito) ToJSON() ([]byte, error) {
+	return json.Marshal(distrito)
+}
+
+func (distrito *Distrito) ToString() string {
+	return "Distrito : { id : " + strconv.Itoa(distrito.ID) +
+		", nombre : '" + distrito.Nombre + "' }"
+}
--- routes/distrito.go
+++ routes/distrito.go
@@ -1,10 +1,26 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-pp/config"
+	"github.com/gin-pp/models"
+)
 
 func DistritoListar(c *gin.Context) {
-	c.Param("provincia_id")
-	c.JSON(200, gin.H{
-		"message": "listar distritos",
-	})
+	provincia_id, err := strconv.ParseInt(c.Param("provincia_id"), 10, 64)
+	if err != nil {
+		c.String(500, "Error: El parámetro enviado no se pudo parsear a entero")
+	} else {
+		var distritos []models.Distrito
+		if err := config.Database().Where("provincia_id = ?", provincia_id).Find(&distritos).Error; err != nil {
+			fmt.Println(err)
+		} else {
+			c.JSON(200, gin.H{
+				"rpta": distritos,
+			})
+		}
+	}
 }
